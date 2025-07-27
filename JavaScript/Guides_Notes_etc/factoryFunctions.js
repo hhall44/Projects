6db89d1728c22e53c7e54e8322c853704996509f
Huston <huston.hall44@gmail.com>
2025-07-27 09:14:52 -0400

    added notes in factory function on ES6 shorthand.

--- JavaScript/Guides_Notes_etc/factoryFunctions.js
+++ JavaScript/Guides_Notes_etc/factoryFunctions.js
@@ -10,3 +10,20 @@ const robotFactory = (model, mobile) => {
 const tinCan = robotFactory('P-500', true);
 tinCan.beep();
 console.log(tinCan);
+
+// short hand in ES6:
+
+const robotFactory = (model, mobile) => {
+  return {
+    model, //omits part of the above "model: model", but still returns the same function. 
+    mobile,
+    beep() {
+      console.log('Beep Boop');
+    }
+  }
+}
+
+// To check that the property value shorthand technique worked:
+const newRobot = robotFactory('P-501', false)
+console.log(newRobot.model)
+console.log(newRobot.mobile)
