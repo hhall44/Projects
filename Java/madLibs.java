ef78d9a5357e7bf8d43ba5a435ab7fd14f972e13
Huston <huston.hall44@gmail.com>
2025-09-06 14:30:58 -0400

    added madLibs.java; more work w/ java variables

---
+++ Java/madLibs.java
@@ -0,0 +1,39 @@
+public class MadLibs {
+  /*
+  This program generates a mad lib style story using java variables.
+  Author: Huston Hall
+  Date: Sep 6, 2025
+  */
+  
+  	public static void main(String[] args){
+      String name1 = "Tristan";
+      String adjective1 = "big";
+      String adjective2 = "happy";
+      String adjective3 = "cold";
+      String verb1 = "running";
+      String noun1 = "teacher";
+      String noun2 = "city";
+      String noun3 = "car";
+      String noun4 = "book";
+      String noun5 = "shoe";
+      String noun6 = "bottle";
+      String name2 = "Sammy";
+      int number = 9;
+      String place1 = "Paris";
+
+      
+
+
+
+
+
+      
+      //The template for the story
+      String story = "This morning "+name1+" woke up feeling "+adjective1+". 'It is going to be a "+adjective2+" day!' Outside, a bunch of "+noun1+"s were protesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place1+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a world where "+noun6+"s ruled the world.";
+
+      System.out.println(story);
+
+    }  
+
+}
+
