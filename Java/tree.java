be29256925696796f5c88a2fa54c24a38959fa01
Huston <huston.hall44@gmail.com>
2025-09-06 10:07:44 -0400

    added tree.java; another example of how to make a class and print to the console

---
+++ Java/tree.java
@@ -0,0 +1,15 @@
+// Define your tree class in this file!
+public class Tree {
+  public static void main(String[] args) {
+    // This method will introduce ourselves and print a tree to the console
+    System.out.println("Hello, my name is Huston and I am learning to code in Java!");
+    System.out.println("I'm going to plant a tree today.");
+    System.out.println("   ⭐️  ");
+    System.out.println("  **** ");
+    System.out.println(" ****** ");
+    System.out.println("********");
+    System.out.println("   **  ");
+    System.out.println("   **  ");
+    System.out.println("--------");
+  }
+}
